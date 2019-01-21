74529629900fba84aa3e888c0e228d8fd968975c
Joan Awinja Ingari <joan.awinja@andela.com>
2019-01-21 11:37:41 +0300

    make one api end point for all calls

--- api/posta/urls.py
+++ api/posta/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('pages', views.facebook_pages, name='facebook_pages'),
+    path('facebook_graph_call', views.facebook_pages, name='facebook_graph_call'),
     path('privacy_policy', views.privacypolicy, name='privacypolicy'),
     path('terms_service', views.termsservice, name='termsservice')
 ]
--- api/posta/views.py
+++ api/posta/views.py
@@ -7,14 +7,21 @@ def index(request):
     if request:
       return HttpResponse("funtimes")
 
+
 @api_view(['POST'])
-def facebook_pages(request):
+def facebook_graph_call(request):
+    """
+    This should cater for all end points
+    https://graph.facebook.com/{your-page-id}/feed?message=Hello%20world!&access_token={your-page-access-token}
+    """
     try:
       page_access_token = request.GET.get('page_access_token')
       facebook_page_id = request.GET.get('facebook_page_id')
-      message = request.GET.get('message')
+      purpose = request.GET.get('purpose') #can be feed, photos, videos,
+      load = request.GET.get('load') #can be message, url, link, source, published etc
+      load_item = request.GET.get('load_item') #is the item you want posted eg Awesome!
       graph = 'https://graph.facebook.com/'
-      r = request.POST(graph + facebook_page_id + '/' + 'feed' + message + '/' + page_access_token)
+      r = request.POST(graph + facebook_page_id + '/' + purpose + '?' + load=load_item + '&' + access_token=page_access_token)
       return HttpResponse(r)
     except request.ConnectionError:
       return("failed to connect")
@@ -22,8 +29,8 @@ def facebook_pages(request):
 
 @api_view(['GET'])
 def privacypolicy(request):
-    return render('Privacy Policy', 'templates/termsfeed-privacy-policy-html.html')
+    return render('Privacy Policy', 'posta/templates/termsfeed-privacy-policy-html.html')
 
 @api_view(['GET'])
 def termsservice(request):
-    return render('Terms of Service', 'templates/ttermsfeed-terms-service-html.html')
+    return render('Terms of Service', 'posta/templates/ttermsfeed-terms-service-html.html')
