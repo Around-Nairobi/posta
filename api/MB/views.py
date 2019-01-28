a7464d1e2cfb11de9538d0e29e07a079c24032b0
Joan Awinja Ingari <joan.awinja@andela.com>
2019-01-28 17:09:32 +0300

    finish the endpoints, work on scrappers

--- api/MB/views.py
+++ api/MB/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from posta.views import content
 
+'''This scrapper gets data from three on the best mentorship scrappers on the net'''
+'''get access token from app and page id from the page'''
+
+page_access_token = "mama"
+facebook_page_id = "mama" ,
+'''
+purpose, load , load_item
+'''
 def index():
     return 'This is Mentor_Bot'
+
+def scrapper_one():
+    return(content(page_access_token, facebook_page_id, 'purpose', 'load ', 'load_item'))
+    
--- api/posta/views.py
+++ api/posta/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from django.shortcuts import render
 import requests
+import time
 from .models import errors
 
 @api_view(['GET'])
@@ -39,10 +40,27 @@ def facebook_graph_call(request):
     else:
           return HttpResponse('Bad Request', status=400)
 
+def content(page_access_token, facebook_page_id, purpose, load , load_item):
+    '''call this function and pass all the content you need to pass'''
+    data = {
+      "page_access_token": page_access_token,
+      "facebook_page_id": facebook_page_id,
+      "purpose": purpose,
+      "load": load,
+      "load_item": load_item
+    }
+    return requests.post('https://aroundnairobi.herokuapp.com/posta/facebook_graph_call', data)
+
+def runner():
+    '''This runner is called by a cron job on heroku that is triggered at certain times of the day
+       It does the simple duty of calling the functions.
+    '''
+    pass
+
 @api_view(['GET'])
 def privacypolicy(request):
-    return render(request, 'posta/templates/termsfeed-privacy-policy-html.html')
+    return render(request, 'termsfeed-privacy-policy-html.html')
 
 @api_view(['GET'])
 def termsservice(request):
-    return render(request, 'posta/templates/ttermsfeed-terms-service-html.html')
+    return render(request, 'termsfeed-terms-service-html.html')
