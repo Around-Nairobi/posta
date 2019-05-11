1bfc406fc8b632927d9f47e9c2a01c6cead960db
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-12 01:35:18 +0300

    no rest for the wicked

--- api1/CROWDIE/views.py
+++ api1/CROWDIE/views.py
@@ -11,6 +11,7 @@ import os
 
 page_access_token = 'EAADZB7Q46ZCksBAFDJdKc4zW4yLTBNnefqFar7WHYJYZCbGFh0jzutTg78ZAwwyND6lQ2AmKrNMlLXAjlXHZAroQsOq0MUnujkKRDWkdahsurgsEall49Nx5vQh6KD0pYeyuJeVKVSz9hXvGydj284RQZBGvdg6H6CxHWUhsxn8liCugWYTOpjfNIfTG0ZAXZCjcp1sep9eZCDAZDZD'
 facebook_page_id = '280294099451467'
+app_secret = 'd745c3af46ebff89398fe51ea939a6bd'
 '''
 purpose, load , load_item
 '''
@@ -41,6 +42,6 @@ def post_on_crowdie_page(request):
     if page_access_token and facebook_page_id:
         purpose, load, load_item = sample_request()
         # print(purpose, load, load_item)
-        return(content(page_access_token, facebook_page_id, purpose, load, load_item))
+        return(content(page_access_token, facebook_page_id, app_secret, purpose, load, load_item))
     else:
         return HttpResponse('Add page access token and facebook page id')
