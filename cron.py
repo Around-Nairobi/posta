cb13e8cc2443e5f310a4053691b46006c0316568
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-22 01:36:15 +0300

    fix cron

--- cron.py
+++ cron.py
@@ -4,7 +4,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def job():
-    print 'This job is run every minute.'
-
+    print('This job is run every minute.'
+)
 
 sched.start()
