dad39b6c4f4a275f4fb4e8749395fef73f1bc64b
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-22 04:33:12 +0300

    changed url

--- api1/posta/urls.py
+++ api1/posta/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('facebook_graph_call', views.facebook_graph_call, name='facebook_graph_call'),
     path('privacy_policy', views.privacypolicy, name='privacypolicy'),
-    path('terms_service', views.termsservice, name='termsservice')
+    path('terms_service', views.termsservice, name='termsservice'),
+    path('email', views.read_email_from_gmail, name='read_email_from_gmail')
 ]
--- api1/posta/views.py
+++ api1/posta/views.py
@@ -1,5 +1,9 @@
 import os
 import facebook
+import smtplib
+import time
+import imaplib
+import email
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from django.shortcuts import render
@@ -103,3 +107,39 @@ def privacypolicy(request):
 @api_view(['GET'])
 def termsservice(request):
     return render(request, 'termsfeed-terms-service-html.html')
+
+ORG_EMAIL   = "@gmail.com"
+FROM_EMAIL  = "ingari61" + ORG_EMAIL
+FROM_PWD    = "ingari@2018"
+SMTP_SERVER = "imap.gmail.com"
+SMTP_PORT   = 993
+
+@api_view(['GET'])
+def read_email_from_gmail():
+      '''Utility to read email from Gmail Using Python'''
+      try:
+        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+        mail.login(FROM_EMAIL,FROM_PWD)
+        mail.select('inbox')
+
+        type, data = mail.search(None, 'ALL')
+        mail_ids = data[0]
+
+        id_list = mail_ids.split()
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
+
+
+        for i in range(latest_email_id,first_email_id, -1):
+            typ, data = mail.fetch(i, '(RFC822)' )
+
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(response_part[1])
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    print('From : ' + email_from + '\n')
+                    print('Subject : ' + email_subject + '\n')
+
+      except Exception as e:
+        print(str(e))
