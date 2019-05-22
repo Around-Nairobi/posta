99497f436adf9261ba3d0d9c1486bb8cfed0726b
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-23 02:21:34 +0300

    one last error

--- api1/posta/views.py
+++ api1/posta/views.py
@@ -143,10 +143,10 @@ def read_email_from_gmail(request):
           for response_part in data:
               if isinstance(response_part, tuple):
                   print('data2', response_part[1])
-                  msg = email.message_from_string(str(response_part[1]))
+                  msg = email.message_from_string(response_part[1])
                   email_subject = msg['subject']
-                  # email_from = msg['from']
-                  # print('From : ' + email_from + '\n')
+                  email_from = msg['from']
+                  print('From : ' + email_from + '\n')
                   print('Subject : ' + email_subject + '\n')
 
       # except Exception as e:
