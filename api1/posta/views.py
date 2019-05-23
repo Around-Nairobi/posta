06c81fc637c1c253380317a3ef10ec460911868c
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-24 02:44:37 +0300

    date

--- api1/posta/views.py
+++ api1/posta/views.py
@@ -154,6 +154,7 @@ def read_email_from_gmail(request):
                   content = {}
                   content[email_from]= email_subject
                   print('date', date)
+                  print('fron', email_from)
                   return HttpResponse(content, date )
                   # print('From : ' + email_from + '\n')
                   # print('Subject : ' + email_subject + '\n')
