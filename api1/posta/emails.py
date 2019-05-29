08f2e6f8f54dbdabdd9abc1594712e802159fd9e
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-29 04:39:18 +0300

    listening to khalid

--- api1/posta/emails.py
+++ api1/posta/emails.py
@@ -1,8 +1,9 @@
 pages = {
-  "tbtia" :["Google <no-reply@accounts.google.com>","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com", ],
-  "crowdie":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
+  "crowdie":["Google <no-reply@accounts.google.com>","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
   "tm":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
   "msafiri":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
   "hschool":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
-  "mb":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"]
+  "mb":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
+  "tbtia" :["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com", ],
+
 }
