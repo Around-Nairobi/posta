cbc1f80d5f039beec47c74fbbc65cbf61169ca0e
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-29 04:23:03 +0300

    listening to khalid

--- api1/posta/views.py
+++ api1/posta/views.py
@@ -168,4 +168,4 @@ def return_page(email_from):
             if email_from in value:
               return key
             else:
-                  return HttpResponse("error")
+                  return "error"
