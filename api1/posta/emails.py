abb131fe151f5b636db5507e4947bb88d418bdf3
Joan Awinja Ingari <joan.awinja@andela.com>
2019-05-30 10:19:56 +0300

    new try

--- api1/posta/emails.py
+++ api1/posta/emails.py
@@ -1,9 +1,51 @@
 pages = {
-  "crowdie":["Google <no-reply@accounts.google.com>","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
-  "tm":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
-  "msafiri":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
-  "hschool":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
-  "mb":["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com"],
-  "tbtia" :["www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com","www.bbc.com", ],
+  "crowdie":[
+    "Google <no-reply@accounts.google.com>",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com"
+    ],
+  "tm":[
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com"
+    ],
+  "msafiri":[
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com"
+    ],
+  "hschool":[
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com"],
+  "mb":[
+    "yo@dev.to",
+    "update@go.facebookmail.com",
+    "no-reply@m.mail.coursera.org",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com"
+    ],
+  "tbtia" :[
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com",
+    "www.bbc.com"
+    ]
 
 }
