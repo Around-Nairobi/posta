0773eded90525d885cf2dee24441df5e5cd731e3
Joan Awinja Ingari <joan.awinja@andela.com>
2019-06-12 02:02:38 +0300

    add nested lookup

--- api1/posta/emails.py
+++ api1/posta/emails.py
@@ -1,52 +1,19 @@
+'''crowdie, tm, msafiri, hschool, mb, tbtia'''
 pages = {
-  "crowdie":[
-    "=?utf-8?Q?The=20DEV=20Community?= <yo@dev.to>",
-    "Google <no-reply@accounts.google.com>",
-    "Facebook IQ <update@go.facebookmail.com>",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com"
-    ],
-  "tm":[
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com"
-    ],
-  "msafiri":[
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com"
-    ],
-  "hschool":[
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com"],
-  "mb":[
-
+    "crowdie":
+    (
+    "<no-reply@accounts.google.com>",
+    "<update@go.facebookmail.com>",
+    "jess@dev.to",
+    "=?utf-8?Q?The=20DEV=20Community?= <yo@dev.to>"
+    ),
+    "tm":("www.bbc.com"),
+    "msafiri":("www.bbc.com"),
+    "hschool":("www.bbc.com"),
+    "mb":
+    (
     "update@go.facebookmail.com",
-    "no-reply@m.mail.coursera.org",
-    "Facebook IQ <update@go.facebookmail.com>",
-    "www.bbc.com",
-    "www.bbc.com"
-    ],
-  "tbtia" :[
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com",
-    "www.bbc.com"
-    ]
-
-}
+    "no-reply@m.mail.coursera.org"
+    ),
+    "tbtia" :("www.bbc.com")
+  }
